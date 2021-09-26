d8b5aad516993b02aa8c3219f601bec6db087a08
adie10 <adienugroho.email@gmail.com>
2021-09-26 21:40:22 +0700

    perbaikan tampilan

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,4 @@
-//navbar
+//navbar toggle
 const toggle = document.querySelector("#toggle");
 const mobile = document.querySelector(".menu-nav");
 
@@ -6,3 +6,15 @@ toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
   mobile.classList.toggle("active");
 });
+
+// auto hide
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-100%";
+  }
+  prevScrollpos = currentScrollPos;
+};
