4e34634bb76a80b18e6834f714647a3088f6e386
SaMadI <adienugroho.email@gmail.com>
2021-11-15 18:43:56 +0700

    menghapus autohide navbar

--- js/navbar.js
+++ js/navbar.js
@@ -8,13 +8,13 @@ toggle.addEventListener("click", () => {
 });
 
 // auto hide
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav").style.top = "0";
-  } else {
-    document.getElementById("nav").style.top = "-200px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("nav").style.top = "0";
+//   } else {
+//     document.getElementById("nav").style.top = "-200px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
